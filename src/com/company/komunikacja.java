a26ff15ccb7c134cc45c1b4918bd4165766b9b1a
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-02 20:42:25 +0100

    usunięty komentarz

---
+++ src/com/company/komunikacja.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class komunikacja {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //int i = sc.nextInt();
+    double d = sc.nextDouble();
+
+        System.out.printf("aaaaa %.4f bbbbb",d);
+    }
+}
