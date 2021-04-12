4b107e4f3ded8d02d7ec83569e0ada708a3e96ce
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-04-12 19:41:20 +0200

    powtorka

--- src/com/company/Powt.java
+++ src/com/company/Powt.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Powt {
 
-    public static void main(String[] args){
+    static void czytanie(){
         Scanner sc = new Scanner(System.in); // liczb
         Scanner st = new Scanner(System.in); // linii
         String s1;
@@ -25,6 +25,18 @@ public class Powt {
         System.out.println("Podaj kolejne imie : ");
         s1 = st.nextLine();
         System.out.println("Podano 2: " + s1);
+    }
+
+    static void nWypisz(String s, int n){
+        for(int i = 0;i<n; i++){
+            System.out.print(s);
+        }
+    }
+
+    public static void main(String[] args){
+        // czytanie();
+        // nWypisz("Tekst", 10);
+
     }
 
 }
